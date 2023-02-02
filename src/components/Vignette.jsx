203b13cb8c45d0030aab753fd34567fdfc92c8b0
jucroizer <81417866+jucroizer@users.noreply.github.com>
2023-02-02 18:21:55 +0100

    review css desktop page accueil

--- src/components/Vignette.jsx
+++ src/components/Vignette.jsx
@@ -3,7 +3,7 @@ import "../styles/Annonce.css"
 
 function Vignette({title, cover, id}) {
     return <Link className="kasa-logement-link" to={'/logement/' + id}>
-        <article className="logement-item-cover" style={{backgroundImage:`url(${cover})`}}>
+        <article className="logement-item-cover" style={{backgroundImage:`url(${cover})`, backgroundRepeat: 'none', backgroundSize:'cover'}}>
             <div className="cover-gradient"></div>
             <p className='logement-item-title'>{title}</p>
         </article>
